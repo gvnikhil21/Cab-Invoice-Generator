660a13df979fe7a000575856d2dff33ead0f6096
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-10-21 12:18:37 +0530

    Added: Welcome message added to test project config

---
+++ src/main/java/com/bridgelabs/cabinvoicegenerator/controller/CabInvoiceGeneratorMain.java
@@ -0,0 +1,14 @@
+package com.bridgelabs.cabinvoicegenerator.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class CabInvoiceGeneratorMain {
+
+	private static final Logger LOGGER = LogManager.getLogger(CabInvoiceGeneratorMain.class);
+
+	public static void main(String[] args) {
+		// welcome message
+		LOGGER.info("Welcome to cab invoice generator");
+	}
+}
