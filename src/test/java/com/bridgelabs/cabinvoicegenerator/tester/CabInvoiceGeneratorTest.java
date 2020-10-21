178f9df123929f826fd0f1189a4940892e5f7416
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-10-21 23:20:37 +0530

    Added: Test enhanced invoice summary generated for given userID with different ride-types (normal or premium) functionality added

--- src/test/java/com/bridgelabs/cabinvoicegenerator/tester/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabs/cabinvoicegenerator/tester/CabInvoiceGeneratorTest.java
@@ -4,47 +4,19 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import com.bridgelabs.cabinvoicegenerator.controller.CabInvoiceGeneratorMain;
-import com.bridgelabs.cabinvoicegenerator.model.InvoiceGenerator;
-import com.bridgelabs.cabinvoicegenerator.model.InvoiceService;
-import com.bridgelabs.cabinvoicegenerator.model.Ride;
+import com.bridgelabs.cabinvoicegenerator.model.*;
 
 public class CabInvoiceGeneratorTest {
-
-	@Test
-	public void givenDistanceAndTime_ShouldReturnTotalFare() {
-		CabInvoiceGeneratorMain cabInvoiceGeneratorMain = new CabInvoiceGeneratorMain();
-		Double cost = cabInvoiceGeneratorMain.calculateTotalFare(10.0, 5.0);
-		Assert.assertEquals(Double.valueOf(105.0), cost);
-
-		cost = cabInvoiceGeneratorMain.calculateTotalFare(0.2, 1.0);
-		Assert.assertEquals(Double.valueOf(5.0), cost);
-	}
-
-	@Test
-	public void givenMultipleRides_ShouldReturnAggregateFare() {
-		CabInvoiceGeneratorMain cabInvoiceGeneratorMain = new CabInvoiceGeneratorMain();
-		Ride rides[] = { new Ride(10.0, 5.0), new Ride(0.2, 1.0), new Ride(5.0, 1.0) };
-		Double cost = cabInvoiceGeneratorMain.calculateAggregateFareForMulltipleRides(rides);
-		Assert.assertEquals(Double.valueOf(161.0), cost);
-	}
-
-	@Test
-	public void givenMultipleRides_ShouldReturnGeneratedInvoice() {
-		CabInvoiceGeneratorMain cabInvoiceGeneratorMain = new CabInvoiceGeneratorMain();
-		Ride rides[] = { new Ride(10.0, 5.0), new Ride(0.2, 1.0), new Ride(5.0, 1.0) };
-		InvoiceGenerator invoice = cabInvoiceGeneratorMain.generateInvoiceForMulltipleRides(rides);
-		InvoiceGenerator invoiceExpected = new InvoiceGenerator(3, 161.0, 161.0 / 3);
-		Assert.assertEquals(invoiceExpected, invoice);
-	}
 
 	@Test
 	public void givenUserId_ShouldReturnGeneratedInvoiceForThatUser() {
 		CabInvoiceGeneratorMain cabInvoiceGeneratorMain = new CabInvoiceGeneratorMain();
 		InvoiceService invoiceService = new InvoiceService();
-		Ride rides[] = { new Ride(10.0, 5.0), new Ride(0.2, 1.0), new Ride(5.0, 1.0) };
+		Ride rides[] = { new Ride(10.0, 5.0, Ride.RideType.NORMAL), new Ride(0.2, 1.0, Ride.RideType.PREMIUM),
+				new Ride(5.0, 1.0, Ride.RideType.NORMAL) };
 		invoiceService.addRideDetails("user1", rides);
-		InvoiceGenerator invoice = cabInvoiceGeneratorMain.generateInvoiceForMulltipleRides(rides);
-		InvoiceGenerator invoiceExpected = new InvoiceGenerator(3, 161.0, 161.0 / 3);
+		InvoiceGenerator invoice = cabInvoiceGeneratorMain.generateInvoiceForGivenUserId("user1", invoiceService);
+		InvoiceGenerator invoiceExpected = new InvoiceGenerator(3, 176.0, 176.0 / 3);
 		Assert.assertEquals(invoiceExpected, invoice);
 	}
 }
