c93ba302e1208597935dbed194f5a7318ba25110
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-10-21 12:47:04 +0530

    Added: Calculate total fare for given distance and time functionality added

--- src/main/java/com/bridgelabs/cabinvoicegenerator/controller/CabInvoiceGeneratorMain.java
+++ src/main/java/com/bridgelabs/cabinvoicegenerator/controller/CabInvoiceGeneratorMain.java
@@ -6,9 +6,18 @@ import org.apache.logging.log4j.Logger;
 public class CabInvoiceGeneratorMain {
 
 	private static final Logger LOGGER = LogManager.getLogger(CabInvoiceGeneratorMain.class);
+	private static final double MINIMUM_FARE = 5.0;
+	private static final double COST_PER_KM = 10.0;
+	private static final double COST_PER_MINUTE = 1.0;
 
 	public static void main(String[] args) {
 		// welcome message
 		LOGGER.info("Welcome to cab invoice generator");
+	}
+
+	public Double calculateTotalFare(double distance, double minutes) {
+		if (distance * COST_PER_KM + minutes * COST_PER_MINUTE < MINIMUM_FARE)
+			return MINIMUM_FARE;
+		return distance * COST_PER_KM + minutes * COST_PER_MINUTE;
 	}
 }
