956a43a22d3f78ce4f8d98d28479ab2814a5289f
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-10-21 23:18:46 +0530

    Added: RideType Normal or Premium added to Ride class

--- src/main/java/com/bridgelabs/cabinvoicegenerator/model/Ride.java
+++ src/main/java/com/bridgelabs/cabinvoicegenerator/model/Ride.java
@@ -3,11 +3,17 @@ package com.bridgelabs.cabinvoicegenerator.model;
 public class Ride {
 	private double distance;
 	private double minutes;
+	private RideType rideType;
+
+	public enum RideType {
+		NORMAL, PREMIUM;
+	}
 
 	// constructor
-	public Ride(double distance, double minutes) {
+	public Ride(double distance, double minutes, RideType rideType) {
 		this.distance = distance;
 		this.minutes = minutes;
+		this.rideType = rideType;
 	}
 
 	// getters
@@ -17,5 +23,9 @@ public class Ride {
 
 	public double getMinutes() {
 		return minutes;
+	}
+
+	public RideType getRideType() {
+		return rideType;
 	}
 }
